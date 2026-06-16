c63b05c2dd48e0513f2073b5b4de7f8df08f7920
upmanyu <upamnyu12@users.noreply.huggingface.co>
2026-06-16 04:26:20 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 THE ADVOCACY A LAW FIRM. All rights reserved.
+# Confidential and proprietary. Do not distribute without a license.
+# THE ADVOCACY A LAW FIRM is the sole owner and title holder of this software.
+
 import uvicorn
 
 if __name__ == "__main__":
