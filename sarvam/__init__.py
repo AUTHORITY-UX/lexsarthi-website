21d9d1c4e381d93ef39db89d67d4a439e3eed763
upmanyu <upamnyu12@users.noreply.huggingface.co>
2026-08-03 20:14:29 +0000

    Delete sarvam

--- sarvam/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Sarvam AI integration package."""
-from .client import SarvamClient, SarvamModel, SarvamMessage, SarvamResponse, sarvam_client
-
-__all__ = ["SarvamClient", "SarvamModel", "SarvamMessage", "SarvamResponse", "sarvam_client"]
